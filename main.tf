071903c8e6f189ded33311de39f187e0929382ed
clubcaliche <cmoreira@misfirm.com>
2024-12-11 11:45:26 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = "rg-shared-services"
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "misfirm_network" {
+  name                = "vnet-shared-10-65-0"
+  address_space       = ["10.65.0.0/20"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnet 1
+resource "azurerm_subnet" "misfirm_subnet_1" {
+  name                 = "snet-shared-10-60-0-0"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.misfirm_network.name
+  address_prefixes     = ["10.65.0.0/24"]
+}
+
+# Subnet 2
+resource "azurerm_subnet" "misfirm_subnet_2" {
+  name                 = "snet-shared-10-60-1-0"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.misfirm_network.name
+  address_prefixes     = ["10.65.1.0/24"]
+}
